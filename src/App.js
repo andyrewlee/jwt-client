35afc8df2896a0438d6f98ea794d62e81bd27c5b
Andrew Lee <andrew.lee@hired.com>
2019-09-23 19:08:31 -0700

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-function App() {
+const LandingPage = () => <h1>Landing Page</h1>
+const LoginPage = () => <h1>Login Page</h1>
+const JoinPage = () => <h1>Join Page</h1>
+const SecretPage = () => <h1>Secret Page</h1>
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login/">Login</Link>
+            </li>
+            <li>
+              <Link to="/join/">Join</Link>
+            </li>
+            <li>
+              <Link to="/secret/">Secret</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/login" exact component={LoginPage} />
+        <Route path="/join" exact component={JoinPage} />
+        <Route path="/secret" exact component={SecretPage} />
+      </div>
+    </Router>
   );
 }
 
